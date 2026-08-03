aad2760e438323d0f24eb5c7f5265f2e626e9849
Priyanshu Jaggi <priyanshujaggi188@gmail.com>
2026-08-03 14:01:42 +0530

    commit part 2

---
+++ services/platform/src/test/java/com/tracedefend/platform/TraceDefendPlatformApplicationTests.java
@@ -0,0 +1,12 @@
+package com.tracedefend.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = TraceDefendPlatformApplication.class)
+class TraceDefendPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
